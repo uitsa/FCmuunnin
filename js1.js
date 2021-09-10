ab9ab3ec39647852053663c2e8e68368707901ab
uitsa <uittosami@gmail.com>
2021-09-10 16:02:45 +0300

    Laskukaava lisätty

--- js1.js
+++ js1.js
@@ -3,11 +3,18 @@ const fahrenheitTuloste = document.querySelector('#fahrenheit');
 const celsiusTuloste = document.querySelector('#celsius');
 const muunnaButton = document.querySelector('button');
 
+function fahrenheitCelsiukseksi(fahrenheit) {
+    let celsius = math.round((fahrenheit -32)*0.5556);
+    
+    return celsius;
+}
+
+
 function muunna() {
     let syote = fahrenheitSyote.value;
     fahrenheitTuloste.textContent = syote;
 
-    celsiusTuloste.textContent = syote;
+    celsiusTuloste.textContent = fahrenheitCelsiukseksi(syote);
 
 }
 
