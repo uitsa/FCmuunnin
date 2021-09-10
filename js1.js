f71ad1776787347cfdc60a90cf6fa506350d9158
uitsa <uittosami@gmail.com>
2021-09-10 15:41:42 +0300

    lisää js:ää

---
+++ js1.js
@@ -0,0 +1,12 @@
+const fahrenheitSyote = document.querySelector('input');
+const fahrenheitTuloste = document.querySelector('#fahrenheit');
+const celsiusTuloste = document.querySelector('#celsius');
+const muunnaButton = document.querySelector('button');
+
+function muunna() {
+    alert('täällä ollaan');
+
+}
+
+
+muunnaButton.addEventListener('click', muunna);
